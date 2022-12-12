219b310e999ac03d501d101e940a5a4e33befbdd
Paul Funston <paulfunston9@gmail.com>
2022-12-12 12:16:32 -0600

    wait for location

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,30 +1,91 @@
 'use strict';
 /*
-  XX
+  Geo Adventure
   Paul Funston
 
-  xx
+  A Geocaching adjacent company
 
 */
 
-// import {select, onEvent, selectAll} from './util.js';
+
+  // Finding Location
 
 
+// let lat = 0;
+// let alt = 0;
+// let long = 0;
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGZ1biIsImEiOiJjbGJncm80Z2MwY2F4M3FwNXpxZjh5cjV6In0.9-biahBbvoH-rpsabZLQgQ';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-97.1, 49.9], // starting position [lng, lat]
-    zoom: 8, // starting zoom
-});
+let long,lat,alt;
 
+function getLocation(position) {
+  // console.log(position);
+  const { altitude, latitude, longitude } = position.coords;
+  alt = altitude ? altitude : undefined;
+  lat = latitude;
+  long = longitude;
+  
+  createMap(long, lat);
+  clearCover();
+};
 
-async function getLocation() {
+function errorHandler(event) {
+  console.log(event);
+  stopSpin();
+  // throw (event.message);
+};
 
+const options = {
+  enableHighAccuracy: true
+};
+
+function requestLocation() {
+
+  // return new Promise((resolve, reject) => {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          getLocation, 
+          errorHandler,
+          options
+          );
+          // resolve()
+      } 
+    } catch (error) {
+      console.log(error.message);
+    }
+  // })
+}
+
+function stopSpin() {
+  const loading = document.querySelector('.loading');
+  loading.style.animation = '';
+};
+
+function clearCover() {
+  const cover = document.querySelector('.map-cover');
+  setTimeout(() => {
+    cover.style.visibility = 'hidden';
+  }, 1000)
 }
 
 
-const marker1 = new mapboxgl.Marker(); 
-  marker1.setLngLat([-97.1, 49.9]);
-  marker1.addTo(map);
+// Using the Map
+
+function createMap(long, lat) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGZ1biIsImEiOiJjbGJncm80Z2MwY2F4M3FwNXpxZjh5cjV6In0.9-biahBbvoH-rpsabZLQgQ';
+  const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [long, lat], // starting position [lng, lat]
+      zoom: 18, // starting zoom
+      pitch: 60,
+  });
+
+  const marker = new mapboxgl.Marker(); 
+  marker.setLngLat([long, lat]);
+  marker.addTo(map);
+};
+
+
+
+requestLocation()
